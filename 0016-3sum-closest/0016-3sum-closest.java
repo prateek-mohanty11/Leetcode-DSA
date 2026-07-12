9019d4e0bc1ac64ca1dca9c37c35d660a02baaee
Prateek Mohanty <prateekmohanty2006@gmail.com>
2026-07-12 17:01:25 +0530

    Time: 15 ms (98.23%), Space: 45.7 MB (14.13%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int diff = Integer.MAX_VALUE;
+        int result = 0;
+
+        for (int i = 0 ; i < n-2 ; i++){
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                int d = Math.abs(target - sum);
+                if (d < diff){
+                    diff = d;
+                    result = sum;
+                }
+                if (sum == target)
+                    return result;
+                else if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return result;
+    }
+}
