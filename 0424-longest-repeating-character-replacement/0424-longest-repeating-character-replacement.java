636df7268ee4e763e0a433480a57d82668b3ecf5
Prateek Mohanty <prateekmohanty2006@gmail.com>
2026-07-12 16:31:37 +0530

    Time: 7 ms (89.42%), Space: 46.3 MB (51.17%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,41 +1,24 @@
 class Solution {
-
-    public int find(int[] freq) {
-        int maxCount = -1;
-
-        for (int i = 0; i < 256; i++) {
-            maxCount = Math.max(maxCount, freq[i]);
-        }
-
-        return maxCount;
-    }
-
     public int characterReplacement(String s, int k) {
-        int n = s.length();
-        int[] freq = new int[256];
+
+        int[] freq = new int[26];
 
         int left = 0;
-        int maxLen = Integer.MIN_VALUE;
+        int maxFreq = 0;
+        int maxLen = 0;
 
-        for (int right = 0; right < n; right++) {
+        for (int right = 0; right < s.length(); right++) {
 
-            freq[s.charAt(right)]++;
+            freq[s.charAt(right) - 'A']++;
 
-            int maxCount = find(freq);
-            int len = right - left + 1;
-            int diff = len - maxCount;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
 
-            while (diff > k) {
-                freq[s.charAt(left)]--;
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
                 left++;
-
-                maxCount = find(freq);
-                len = right - left + 1;
-                diff = len - maxCount;
             }
 
-            len = right - left + 1;
-            maxLen = Math.max(maxLen, len);
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
         return maxLen;
