4a1407520ebd94752c2d80089fd839a5f573d2c5
Prateek Mohanty <prateekmohanty2006@gmail.com>
2026-08-22 22:20:09 +0530

    Time: 1 ms (78.16%), Space: 42.2 MB (89.24%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,17 +1,17 @@
 class Solution {
-    private int next(int num) {
+    private int next(int num){
         int sum = 0;
-        while (num > 0) {
-            int digit = num % 10;// Extract the last digit
-            sum += digit * digit;// Add its square
-            num /= 10;// Remove the last digit
+        while(num > 0){
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
         }
         return sum;
     }
     public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
-        while (fast != 1){
+        while(fast != 1){
             slow = next(slow);
             fast = next(next(fast));
             if(slow == fast && slow != 1){
